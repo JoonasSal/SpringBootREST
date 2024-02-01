e5f98e2adda5e383d039f44877729f0a76b21e89
JoonasSal <122926329+JoonasSal@users.noreply.github.com>
2024-02-01 22:35:53 +0200

    luotu hangmanService

--- restapi/src/main/java/com/joosal/restapi/hangmanService.java
+++ restapi/src/main/java/com/joosal/restapi/hangmanService.java
@@ -1,5 +1,25 @@
 package com.joosal.restapi;
 
-public class hangmanService {
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
+public class hangmanService {
+    private Map<Integer, Words> words = new HashMap<>();
+    private Map<String, Player> players = new HashMap<>();
+
+    // metodi arvattavan sanan lisäämiseksi
+    public void addWord(Words word) {
+        words.put(word.getId(), word);
+    }
+
+    // metodi arvattavan sanan hakemiseen id:llä
+    public Words getWord(int id) {
+        return words.get(id);
+    }
+    
+    public Collection<Words> getAllWords() {
+        return words.values();
+    }
+    
 }
