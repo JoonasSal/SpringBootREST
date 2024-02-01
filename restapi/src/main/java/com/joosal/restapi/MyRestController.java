5b45a51f02914f616e3b7e60dfb6db452b26653f
JoonasSal <122926329+JoonasSal@users.noreply.github.com>
2024-02-01 22:01:29 +0200

    luennolta värkätty jotain

--- restapi/src/main/java/com/joosal/restapi/MyRestController.java
+++ restapi/src/main/java/com/joosal/restapi/MyRestController.java
@@ -2,6 +2,11 @@ package com.joosal.restapi;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 
@@ -10,7 +15,28 @@ public class MyRestController {
 
     @GetMapping("/")
     public String getHomepage() {
+        System.out.println("get pyyntö juureen");
         return "Welcome to REST root";
     }
+
+    @GetMapping("/user")
+    public String user(@RequestParam String username) {
+        return username;
+    }
+    
+
+    @PostMapping("/user")
+    public String addUser(@RequestParam String username) {
+        
+        System.out.println("Adding user " + username);
+        return "Success";
+    }
+
+    @GetMapping("/user/{id}")
+    public String getUserById(@PathVariable Integer id) {
+        return id.toString();
+    }
+    
+    
     
 }
